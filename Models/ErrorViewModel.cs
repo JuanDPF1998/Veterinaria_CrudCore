f6c4a588a7d2853b903761f99d29f5893dd11629
Juan DPF1998 <JuanDanielPF1998@gmail.com>
2025-08-17 09:22:13 -0600

    Proyecto vacio veterinaria

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Veterinaria_crudcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
