933d3a66bd5d1a8265624f25713e55bf300f9431
Juan DPF1998 <JuanDanielPF1998@gmail.com>
2025-08-18 11:36:40 -0600

    2_se agrego clase Modelo

---
+++ Models/Mascotas.cs
@@ -0,0 +1,11 @@
+﻿namespace Veterinaria_crudcore.Models
+{
+    public class Mascotas
+    {
+        public int Id { get; set; }
+        public string nombre { get; set; } = string.Empty;
+        public string raza { get; set; } = string.Empty;
+        public string generoRaza { get; set; } = string.Empty;
+        public string descripcion { get; set; } = string.Empty;
+    }
+}
