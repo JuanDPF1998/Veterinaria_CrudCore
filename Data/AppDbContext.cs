20069f7429ecde756fb77d3776ee6a95efd8ca9f
Juan DPF1998 <JuanDanielPF1998@gmail.com>
2025-08-18 11:40:24 -0600

    3_Data archivó

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Veterinaria_crudcore.Models;
+
+namespace Veterinaria_crudcore.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options): base(options) { }
+        public DbSet<Mascotas> mascotas { get; set; }
+    }
+}
